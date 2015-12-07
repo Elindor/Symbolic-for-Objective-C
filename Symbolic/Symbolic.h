66fccf48311da32f3654f6c29bee7e748a24083d
Elindor <ganopper@gmail.com>
2015-12-07 13:23:25 -0200

    Initial Commit

---
+++ Symbolic/Symbolic.h
@@ -0,0 +1,13 @@
+//
+//  Symbolic.h
+//  Symbolic
+//
+//  Created by Gabriel Nopper on 07/12/15.
+//  Copyright © 2015 Gabriel Nopper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Symbolic : NSObject
+
+@end
---
+++ Symbolic/Symbolic.m
@@ -0,0 +1,13 @@
+//
+//  Symbolic.m
+//  Symbolic
+//
+//  Created by Gabriel Nopper on 07/12/15.
+//  Copyright © 2015 Gabriel Nopper. All rights reserved.
+//
+
+#import "Symbolic.h"
+
+@implementation Symbolic
+
+@end
